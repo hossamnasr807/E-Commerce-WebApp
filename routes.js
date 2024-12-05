52240e10f2a67c48d1c6be5d203f9617ebcb2ef4
Hossam Nasr Mohamed <84002927+hossamnasr807@users.noreply.github.com>
2024-12-05 05:44:07 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import ProductsPage from "./pages/ProductsPage";
-
+import LoginPage from "./pages/loginpage";
+import LoginForm from "./components/loginform";
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
